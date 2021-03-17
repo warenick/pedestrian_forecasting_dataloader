f3c54ca31af2039bd652178092f44e00a432a52e
alexpostnikov <alex.postnikov@skolkovotech.ru>
2021-03-17 12:05:34 +0300

    merge

--- dataloader_test.py
+++ dataloader_test.py
@@ -1,63 +1,54 @@
 import numpy as np
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageOps
 import matplotlib.pyplot as plt
-from scripts.config import cfg
-from scripts.dataloader import DatasetFromTxt
+from config import cfg
+from dataloader import DatasetFromTxt
+from utils import transform_points
 from tqdm import tqdm
+
 import torch
 
 draw_n = 1
-draw_a_h = 0
+draw_a_h = 1
 draw_speeds = 0
-draw_targets = 0
+draw_targets = 1
 once = 1
-if __name__ == "__main__":
 
-    files = ["stanford/bookstore_0.txt", "stanford/bookstore_1.txt", "stanford/bookstore_2.txt",
-             "stanford/bookstore_3.txt", "stanford/coupa_3.txt", "stanford/deathCircle_0.txt",
-             "stanford/deathCircle_1.txt", "stanford/deathCircle_2.txt",
-             "stanford/deathCircle_3.txt", "stanford/deathCircle_4.txt",
-             "stanford/gates_0.txt", "stanford/gates_1.txt", "stanford/gates_3.txt", "stanford/gates_4.txt",
-             "stanford/gates_5.txt", "stanford/gates_6.txt", "stanford/gates_7.txt", "stanford/gates_8.txt",
-             "stanford/hyang_5.txt", "stanford/hyang_6.txt", "stanford/hyang_7.txt", "stanford/hyang_9.txt",
-             "stanford/nexus_1.txt", "stanford/nexus_3.txt", "stanford/nexus_4.txt", "stanford/nexus_7.txt",
-             "stanford/nexus_8.txt", "stanford/nexus_9.txt"]
-
-    path_ = "../data/train/"
-    dataset = DatasetFromTxt(path_, files, cfg, use_forces=True, forces_file="forces_19.02.txt")
-
-
-    #### VISUALIZE TRAJECTORIES
-
-    # radius of point to be plotted
+def visualize_test():
     Rad = 2
-    path_to_save = "/home/robot/repos/SDD_forces/192_192_f_n/"
+    cfg["raster_params"]["use_map"] = True
+    cfg["raster_params"]["normalize"] = True
+    files = [
+        "eth_hotel/eth_hotel.txt"
+    ]
+    path_ = "./data/train/"
+    dataset = DatasetFromTxt(path_, files, cfg)
+    # path_to_save = "/home/robot/repos/SDD_forces/192_192_f_n/"
     for i in tqdm(range(0, len(dataset))):
-        data = dataset[i]
-        # np.save(path_to_save+str(i), data)
-        # continue
-        #
-        # if data["img"] is np.array([None]):
-        #     continue
+        ind = int(np.random.rand() * len(dataset))
+        data = dataset[ind]
+
         img = Image.fromarray(np.asarray(data["img"], dtype="uint8"))
         draw = ImageDraw.Draw(img)
-        # if np.sum(data["target_avil"]) >= 3:
-        #     continue
 
         if draw_a_h:
             for num, pose in enumerate(data["agent_hist"]):
                 if data["agent_hist_avail"][num]:
                     # pose = data["glob_to_local"] @ np.array([pose[0], pose[1], 1.])
                     pose_raster = data["raster_from_agent"] @ np.array([pose[0], pose[1], 1.])
-                    draw.ellipse((pose_raster[0] - Rad, pose_raster[1] - Rad, pose_raster[0] + Rad, pose_raster[1] + Rad),
-                                 fill='red', outline='red')
+                    draw.ellipse(
+                        (pose_raster[0] - Rad, pose_raster[1] - Rad, pose_raster[0] + Rad, pose_raster[1] + Rad),
+                        fill='red', outline='red')
                     # if num == 0:
                     if draw_speeds:
                         import cv2
                         na = np.asarray(img)
-                        if (np.sum(data["agent_speed"][num]**2) > 1e-6) and (data["agent_hist_avail"][num] * data["agent_hist_avail"][num+1]):
+                        if (np.sum(data["agent_speed"][num] ** 2) > 1e-6) and (
+                                data["agent_hist_avail"][num] * data["agent_hist_avail"][num + 1]):
                             speed = data["agent_speed"][num]
-                            na = cv2.arrowedLine(na, (int(pose_raster[0]), int(pose_raster[1])), (int(pose_raster[0]+(speed[0]*300)), int(pose_raster[1]+(speed[1]*300))), (0, 0, 0), 8)
+                            na = cv2.arrowedLine(na, (int(pose_raster[0]), int(pose_raster[1])), (
+                            int(pose_raster[0] + (speed[0] * 300)), int(pose_raster[1] + (speed[1] * 300))), (0, 0, 0),
+                                                 8)
                             img = Image.fromarray(np.asarray(na, dtype="uint8"))
                             draw = ImageDraw.Draw(img)
 
@@ -65,11 +56,12 @@ if __name__ == "__main__":
             for ped in range(0, data["neighb"].shape[0]):
                 for num, pose in enumerate(data["neighb"][ped, :, :]):
                     if data["neighb_avail"][ped, num]:
-
                         pose_raster = data["raster_from_agent"] @ np.array([pose[0], pose[1], 1.])
-                        rgb = (int(255*((num+1)/8)), int(255*((num+1)/8)), int(255*((num+1)/8)))
+                        rgb = (int(255 * ((num + 1) / 8)), int(255 * ((num + 1) / 8)), int(255 * ((num + 1) / 8)))
                         # draw.ellipse((pose_raster[0] - Rad, pose_raster[1] - Rad, pose_raster[0] + Rad, pose_raster[1] + Rad), fill='#ffcc99', outline='#ffcc99')
-                        draw.ellipse((pose_raster[0] - Rad, pose_raster[1] - Rad, pose_raster[0] + Rad, pose_raster[1] + Rad), fill=rgb, outline='#ffcc99')
+                        draw.ellipse(
+                            (pose_raster[0] - Rad, pose_raster[1] - Rad, pose_raster[0] + Rad, pose_raster[1] + Rad),
+                            fill=rgb, outline='#ffcc99')
 
         if draw_targets:
             for num, pose in enumerate(data["target"]):
@@ -77,13 +69,49 @@ if __name__ == "__main__":
                     R = 4
                     # pose = data["glob_to_local"] @ np.array([pose[0], pose[1], 1.])
                     pose_raster = data["raster_from_agent"] @ np.array([pose[0], pose[1], 1.])
-                    draw.ellipse((pose_raster[0] - Rad, pose_raster[1] - Rad, pose_raster[0] + Rad, pose_raster[1] + Rad), fill='#33cc33', outline='#33cc33')
-        data["img"] = np.asarray(img)
-        np.save(path_to_save + str(i), data)
-        if once:
-            once = False
-            plt.imshow(img)
-            plt.savefig(path_to_save+str(i)+".jpg")
-        # plt.close()
-        # exit()
-        # plt.show()
+                    draw.ellipse(
+                        (pose_raster[0] - Rad, pose_raster[1] - Rad, pose_raster[0] + Rad, pose_raster[1] + Rad),
+                        fill='#33cc33', outline='#33cc33')
+        img.show()
+        print()
+if __name__ == "__main__":
+    from train_test_split import get_train_val_dataloaders
+    from dataloader import  collate_wrapper
+    import numpy as np
+    cfg["raster_params"]["use_map"] = False
+    cfg["raster_params"]["normalize"] = True
+    path_ = "/media/robot/hdd1/hdd_repos/pedestrian_forecasting_dataloader/data/train/"
+    val_files = ["eth_hotel", "biwi_eth", "zara01", "zara02", "students"]
+    for val_file in val_files:
+        train_ds, val_ds = get_train_val_dataloaders(path_, cfg, val_file, False)
+        from torch.utils.data import DataLoader
+        train_dataloader = DataLoader(train_ds, batch_size=512,
+                                      shuffle=True, num_workers=0, collate_fn=collate_wrapper)
+        val_dataloader = DataLoader(val_ds, batch_size=512,
+                                    shuffle=False, num_workers=0, collate_fn=collate_wrapper)
+
+        train_poses = np.zeros((0,2))
+        val_poses = np.zeros((0, 2))
+        for num,data in enumerate(tqdm(train_dataloader)):
+            train_poses = np.concatenate((train_poses, data.history_positions[:,:,:2][data.history_av != 0].reshape(-1,2)))
+            if num>10:
+                break
+        for num,data in enumerate(tqdm(val_dataloader)):
+            val_poses = np.concatenate((val_poses, data.history_positions[:,:,:2][data.history_av != 0].reshape(-1,2)))
+            if num>10:
+                break
+
+        n_bins = 60
+
+        plt.hist(val_poses[:, 0], n_bins, alpha=0.5, label='val_poses')
+        plt.hist(train_poses[:,0], n_bins, alpha=0.5, label='train_poses')
+        plt.legend(loc='upper right')
+        plt.savefig("tests/"+val_file+'_x.png')
+        plt.show()
+        plt.close()
+        plt.hist(val_poses[:, 1], n_bins, alpha=0.5, label='val_poses')
+        plt.hist(train_poses[:, 1], n_bins, alpha=0.5, label='train_poses')
+        plt.legend(loc='upper right')
+        plt.savefig("tests/"+val_file + '_y.png')
+        plt.show()
+
