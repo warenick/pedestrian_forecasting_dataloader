0914d3f4b57b975bf3fb56f18de0ab13a12171db
alexpostnikov <alex.postnikov@skolkovotech.ru>
2021-05-20 13:41:51 +0300

    __init__.py caused errors

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-
-import torch
-from tqdm import tqdm
-from train_utils import setup_experiment, distance,get_lr
-from pedestrian_forecasting_dataloader.dataloader import collate_wrapper, DatasetFromTxt
-from pedestrian_forecasting_dataloader.train_test_split import get_train_val_dataloaders
-from pedestrian_forecasting_dataloader.utils import vis_image_batched_Aleksander, preprocess_data
+#
+# import torch
+# from tqdm import tqdm
+# # from train_utils import setup_experiment, distance,get_lr
+# from pedestrian_forecasting_dataloader.dataloader import collate_wrapper, DatasetFromTxt
+# from pedestrian_forecasting_dataloader.train_test_split import get_train_val_dataloaders
+# from pedestrian_forecasting_dataloader.utils import vis_image_batched_Aleksander, preprocess_data
